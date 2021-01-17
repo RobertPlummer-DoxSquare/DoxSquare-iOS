cff177be1cda939cfea6ea93aa3f547add3d3942
Bobby Plummer <tagtechinc@gmail.com>
2021-01-16 21:51:11 -0500

    Initial Commit

---
+++ DoxSquare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoxSquare
+//
+//  Created by Robert Plummer on 1/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
